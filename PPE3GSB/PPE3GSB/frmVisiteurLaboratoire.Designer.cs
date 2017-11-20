e795d958bd78982ef80aced08ce611590849fe53
delphine <delphine.duffo@gmail.com>
2017-11-20 14:55:05 +0100

    Validation test

---
+++ PPE3GSB/PPE3GSB/frmVisiteurLaboratoire.Designer.cs
@@ -0,0 +1,159 @@
+﻿namespace PPE3GSB
+{
+    partial class frmVisiteurLaboratoire
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.lblLaboratoire = new System.Windows.Forms.Label();
+            this.cboLaboratoire = new System.Windows.Forms.ComboBox();
+            this.bsVisiteur = new System.Windows.Forms.BindingSource(this.components);
+            this.dgvVisiteur = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.bsLaboratoire = new System.Windows.Forms.BindingSource(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.bsVisiteur)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVisiteur)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bsLaboratoire)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lblLaboratoire
+            // 
+            this.lblLaboratoire.AutoSize = true;
+            this.lblLaboratoire.Location = new System.Drawing.Point(40, 21);
+            this.lblLaboratoire.Name = "lblLaboratoire";
+            this.lblLaboratoire.Size = new System.Drawing.Size(66, 13);
+            this.lblLaboratoire.TabIndex = 0;
+            this.lblLaboratoire.Text = "Laboratoire :";
+            // 
+            // cboLaboratoire
+            // 
+            this.cboLaboratoire.FormattingEnabled = true;
+            this.cboLaboratoire.Location = new System.Drawing.Point(138, 18);
+            this.cboLaboratoire.Name = "cboLaboratoire";
+            this.cboLaboratoire.Size = new System.Drawing.Size(121, 21);
+            this.cboLaboratoire.TabIndex = 1;
+            // 
+            // bsVisiteur
+            // 
+            this.bsVisiteur.DataSource = typeof(PPE3GSB.Visiteur);
+            // 
+            // dgvVisiteur
+            // 
+            this.dgvVisiteur.AutoGenerateColumns = false;
+            this.dgvVisiteur.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvVisiteur.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6,
+            this.dataGridViewTextBoxColumn7,
+            this.dataGridViewTextBoxColumn8});
+            this.dgvVisiteur.DataSource = this.bsVisiteur;
+            this.dgvVisiteur.Location = new System.Drawing.Point(43, 87);
+            this.dgvVisiteur.Name = "dgvVisiteur";
+            this.dgvVisiteur.Size = new System.Drawing.Size(644, 220);
+            this.dgvVisiteur.TabIndex = 3;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "nom";
+            this.dataGridViewTextBoxColumn3.HeaderText = "nom";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "prenom";
+            this.dataGridViewTextBoxColumn4.HeaderText = "prenom";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "rue";
+            this.dataGridViewTextBoxColumn5.HeaderText = "rue";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.DataPropertyName = "cp";
+            this.dataGridViewTextBoxColumn6.HeaderText = "cp";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            // 
+            // dataGridViewTextBoxColumn7
+            // 
+            this.dataGridViewTextBoxColumn7.DataPropertyName = "ville";
+            this.dataGridViewTextBoxColumn7.HeaderText = "ville";
+            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            // 
+            // dataGridViewTextBoxColumn8
+            // 
+            this.dataGridViewTextBoxColumn8.DataPropertyName = "dateEmbauche";
+            this.dataGridViewTextBoxColumn8.HeaderText = "dateEmbauche";
+            this.dataGridViewTextBoxColumn8.Name = "dataGridViewTextBoxColumn8";
+            // 
+            // bsLaboratoire
+            // 
+            this.bsLaboratoire.DataSource = typeof(PPE3GSB.Laboratoire);
+            // 
+            // frmVisiteurLaboratoire
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(729, 558);
+            this.Controls.Add(this.dgvVisiteur);
+            this.Controls.Add(this.cboLaboratoire);
+            this.Controls.Add(this.lblLaboratoire);
+            this.Name = "frmVisiteurLaboratoire";
+            this.Text = "frmVisiteurLaboratoire";
+            this.Load += new System.EventHandler(this.frmVisiteurLaboratoire_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.bsVisiteur)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvVisiteur)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.bsLaboratoire)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblLaboratoire;
+        private System.Windows.Forms.ComboBox cboLaboratoire;
+        private System.Windows.Forms.BindingSource bsVisiteur;
+        private System.Windows.Forms.DataGridView dgvVisiteur;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn8;
+        private System.Windows.Forms.BindingSource bsLaboratoire;
+    }
+}
